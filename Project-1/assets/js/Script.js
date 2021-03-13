37b7900d5c87fc7bd37bf236d6f49161773bb59d
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-03-13 13:18:07 -0800

    start changing script js

--- Project-1/assets/js/Script.js
+++ Project-1/assets/js/Script.js
@@ -1,41 +1,29 @@
-/** API STUFF */
-// const QAPI_OBJECT = {
-//     qOfDayURL: "https://api.paperquotes.com/apiv1/qod/",
-//     url: "https://api.paperquotes.com/apiv1/quotes/", //? should be first
-//     tagName: "tags=", //comma-separated
-//     langCode: "lang=",
-//     authorName: "author=",
+/** ------ DOM REFERENCES ----- */
+const nav_quiz 			= $("#quiz");
+const nav_profiles 		= $("#profiles");
+const nav_qotd 			= $("#qotd");
 
-//     apiKey: "8b3b2f24c77c3db7ed3487def36faa730228f18a"
-// }
-// var QajaxCallObj = { 
-//     type : "GET", 
-//     url : "https://api.paperquotes.com/apiv1/quotes/", 
-//     beforeSend: function(xhr){
-//         xhr.setRequestHeader('Authorization', `Token ${QAPI_OBJECT.apiKey}` );
-//     },
-//     success : function(result) { 
-//         console.log(result.results); 
-//     }, 
-//     error : function(result) { 
-//         //handle the error
-//         console.log(result);
-//     } 
-//   }
-  // WikiMedia things
-  // Reframe qAPI as fetch with header, not AJAX call
-/** ---------------------------- */
+const carousel 			= $(".quote-carousel");
+const carouselQuote 	= $(".quote");
+/** --------------------------- */
+/** ----- PAGE REFERENCES ----- */
+const URL_QUIZ 			= "/quiz.html";
+const URL_PROFILES 		= "/philosophers.html";
+const URL_QOTD 			= "/qotd.html";
 
+/** -------- LISTENERS -------- */
+//nav bar links
+nav_quiz.on("click", function(e){
+	window.location.assign(URL_QUIZ)
+});
+nav_profiles.on("click", function(e){
+	window.location.assign(URL_PROFILES)
+});
+nav_qotd.on("click", function(e){
+	window.location.assign(URL_QOTD)
+});
 
-const nav_quiz = $("#quiz");
-const nav_profiles = $("#profiles");
-const nav_qotd = $("#qotd");
-
-const carousel = $(".quote-carousel");
-const carouselQuote = $(".quote");
-
-
-
+/** --------------------------- */
 
 
 
