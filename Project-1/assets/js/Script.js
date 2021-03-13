6c627fcd17b52442e78c0677e3ebd31bb787bfc4
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-03-13 10:43:03 -0800

    Add API JS

--- Project-1/assets/js/Script.js
+++ Project-1/assets/js/Script.js
@@ -1,39 +1,27 @@
-
-
-
-
-
-
 /** API STUFF */
+// const QAPI_OBJECT = {
+//     qOfDayURL: "https://api.paperquotes.com/apiv1/qod/",
+//     url: "https://api.paperquotes.com/apiv1/quotes/", //? should be first
+//     tagName: "tags=", //comma-separated
+//     langCode: "lang=",
+//     authorName: "author=",
 
-const QAPI_OBJECT = {
-    qOfDayURL: "https://api.paperquotes.com/apiv1/qod/",
-    
-    url: "https://api.paperquotes.com/apiv1/quotes/", //? should be first
-    tagName: "tags=", //comma-separated
-    langCode: "lang=",
-    authorName: "author=",
-
-    apiKey: "8b3b2f24c77c3db7ed3487def36faa730228f18a"
-}
-
-var QajaxCallObj = { 
-    type : "GET", 
-    url : "https://api.paperquotes.com/apiv1/quotes/", 
-    beforeSend: function(xhr){
-        xhr.setRequestHeader('Authorization', `Token ${QAPI_OBJECT.apiKey}` );
-    },
-    success : function(result) { 
-        console.log(result.results); 
-    }, 
-    error : function(result) { 
-        //handle the error
-        console.log(result);
-    } 
-  }
-
-
-  //TODO: WikiMedia things
-  //TODO: Reframe qAPI as fetch with header, not AJAX call
-
+//     apiKey: "8b3b2f24c77c3db7ed3487def36faa730228f18a"
+// }
+// var QajaxCallObj = { 
+//     type : "GET", 
+//     url : "https://api.paperquotes.com/apiv1/quotes/", 
+//     beforeSend: function(xhr){
+//         xhr.setRequestHeader('Authorization', `Token ${QAPI_OBJECT.apiKey}` );
+//     },
+//     success : function(result) { 
+//         console.log(result.results); 
+//     }, 
+//     error : function(result) { 
+//         //handle the error
+//         console.log(result);
+//     } 
+//   }
+  // WikiMedia things
+  // Reframe qAPI as fetch with header, not AJAX call
 /** ---------------------------- */
