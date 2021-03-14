796f812682f936b1b76bb65e9b5302c3a0305956
breyera <alicia_breyer@yahoo.com>
2021-03-14 03:43:23 -0700

    slowed down carousel

--- Project-1/assets/js/quote-carousel.js
+++ Project-1/assets/js/quote-carousel.js
@@ -17,8 +17,13 @@ $(".owl-carousel").on("initialized.owl.carousel", () => {
   
   const $owlCarousel = $(".owl-carousel").owlCarousel({
     items: 1,
+    slideSpeed : 5000,
+    smartSpeed: 1500,
+    autoplayTimeout: 10000,
     loop: true,
     nav: true,
+    autoplay: true,
+    // autoplayHoverPause:true,
     navText: [
       '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
       '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>' /* icons from https://iconmonstr.com */
