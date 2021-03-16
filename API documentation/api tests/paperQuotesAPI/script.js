092f1d248c4fce86048cf0197a69664d4d5ac20b
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-03-15 20:59:51 -0700

    MOVE API CODE TO API JS

--- API documentation/api tests/paperQuotesAPI/script.js
+++
@@ -1,7 +0,0 @@
-
-async function fetchQuotes() {
-    var response = await fetch('http://philosophy-quotes-api.glitch.me/quotes')
-    var quotes = (response.json())
-    return quotes
-}
-fetchQuotes()
