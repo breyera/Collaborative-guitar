0bc29cfc5b5f04400fe81e19f72e2d220e68a9b4
Trey Love <theendurinpupil@gmail.com>
2021-03-18 16:36:13 -0700

    added picture info in the url of the philosopher prof page

--- Project-1/assets/js/philosophersPage.js
+++ Project-1/assets/js/philosophersPage.js
@@ -1,7 +1,8 @@
 $(".card").on("click", function () {
     var profInfo = $(this).children(".details").children("h2").text()
+    var imgInfo = ($(this).children(".imgBx").children("img").attr("src"))
     console.log(profInfo)
-    var profUrl = "./philosopher-profile.html?" + profInfo
+    var profUrl = "./philosopher-profile.html?" + profInfo + "?" + imgInfo
     window.location.assign(profUrl)
-    console.log("hello")
+
 })
