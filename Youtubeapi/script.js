6e76862eac87f74439321ea3948057e9613e4247
Stuart Walsh <stuartwalsh97@yahoo.com>
2021-03-17 21:26:09 -0700

    now returns embed links

--- Youtubeapi/script.js
+++ Youtubeapi/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-        $('#search-term').submit(function(event){
+        $("#search-term").submit(function(event){
           event.preventDefault();
-          var searchTerm = $('#query').val();
+          var searchTerm = $("#query").val();
           getRequest(searchTerm);
         });
       
@@ -10,12 +10,12 @@ $(document).ready(function(){
       function getRequest(searchTerm){
         var params = {
           "q": "searchTerm",
-          "part": 'snippet',
-          "type": 'video',
-          "embedded": 'true',
-          "key": 'AIzaSyA-PERyp6RWtIztoWvv4dynZlpds03hj-o'
+          "part": "snippet",
+          "type": "video",
+          "embedded": "true",
+          "key": "AIzaSyA-PERyp6RWtIztoWvv4dynZlpds03hj-o"
         }
-        url = 'https://www.googleapis.com/youtube/v3/search';
+        url = "https://www.googleapis.com/youtube/v3/search";
       
         $.getJSON(url, params, function(data){
           showResults(data.items);
@@ -26,10 +26,10 @@ $(document).ready(function(){
         var html = "";
       
         $.each(results, function(index,value){
-          html += '<p>' + value.snippet.thumbnails.high.url + '</p>' + '<p>' + 'https://www.youtube.com/watch?v=' + value.id.videoId + '</p>' + '<hr/>';
+          html += "<p>" + value.snippet.thumbnails.high.url + "</p>" + "<p>" + "https://www.youtube.com/watch?v=" + value.id.videoId + "</p>" + "<hr/>";
           console.log(value.snippet.thumbnails.high.url);
           console.log(value);
         })
-        $('#search-results').html(html);
+        $("#search-results").html(html);
       }
       })
