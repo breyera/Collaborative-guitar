fea71909f78b084ec586df4f7537361096e8448e
Trey Love <theendurinpupil@gmail.com>
2021-03-19 14:21:33 -0700

    the url for the philosopher profile now contains info for the image of the philosopher

--- Project-1/assets/js/philosophersPage.js
+++ Project-1/assets/js/philosophersPage.js
@@ -2,7 +2,7 @@ $(".card").on("click", function () {
     var profInfo = $(this).children(".details").children("h2").text()
     var imgInfo = ($(this).children(".imgBx").children("img").attr("src"))
     console.log(profInfo)
-    var profUrl = "./philosopher-profile.html?" + profInfo + "?" + imgInfo
+    var profUrl = "./philosopher-profile.html?" + profInfo // + "?" + imgInfo
     window.location.assign(profUrl)
 
 })
